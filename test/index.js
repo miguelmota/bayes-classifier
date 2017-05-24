504ab537aa6b12f242e4c1b94252d38f63523e95
Miguel Mota <miguelmota2@gmail.com>
2017-05-24 10:50:25 -0700

    add test

---
+++ test/index.js
@@ -0,0 +1,35 @@
+var test = require('tape')
+
+var BayesClassifier = require('../index')
+
+test(function(t) {
+  t.plan(4)
+
+  var classifier = new BayesClassifier()
+
+  var positiveDocuments = [
+    `I love tacos.`,
+    `Dude, that burrito was epic!`,
+    `Holy cow, these nachos are so good and tasty.`,
+    `I am drooling over the awesome bean and cheese quesadillas.`
+  ]
+
+  var negativeDocuments = [
+    `Gross, worst taco ever.`,
+    `The buritos gave me horrible diarrhea.`,
+    `I'm going to puke if I eat another bad nacho.`,
+    `I'd rather die than eat those nasty enchiladas.`
+  ]
+
+  classifier.addDocuments(positiveDocuments, 'positive')
+  classifier.addDocuments(negativeDocuments, 'negative')
+
+  classifier.train()
+
+  t.equal(classifier.classify(`I heard the mexican restaurant is great!`), 'positive')
+  t.equal(classifier.classify(`I don't want to eat there again.`), 'negative')
+  t.equal(classifier.classify(`The torta is epicly bad.`), 'negative')
+  t.equal(classifier.classify(`The torta is horribly awesome.`), 'positive')
+
+  //console.log(classifier.getClassifications('Burritos are the meaning of life.'));
+})
